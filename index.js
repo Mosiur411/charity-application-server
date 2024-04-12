cb6bdd4c9b4e5f79b49e0a975590c2ad68e3b039
Hazrat02 <hazratbd80@gmail.com>
2024-04-12 01:50:10 -0700

    new code

--- index.js
+++ index.js
@@ -13,17 +13,14 @@ app.use(cors());
 
 app.post("/order", async (req, res) => {
   try {
-    console.log(process.env?.RAZORPAY_KEY_ID)
     const razorpay = new Razorpay({
       key_id: process.env.RAZORPAY_KEY_ID,
       key_secret: process.env.RAZORPAY_SECRET,
     });
+
     let options = req.body;
     options.amount = Number(options?.amount) * 100
     const order = await razorpay.orders.create(options);
-
-
-    
 
     if (!order) {
       return res.status(500).send("Error");
